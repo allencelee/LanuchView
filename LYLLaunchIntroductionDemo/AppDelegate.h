135ec762ecfc5dd45d22ce7c3934bfa138a5a27c
liyalong <liyl@zb-software.com>
2017-12-13 11:09:48 +0800

    提交代码

---
+++ LYLLaunchIntroductionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYLLaunchIntroductionDemo
+//
+//  Created by Lee on 16/4/7.
+//  Copyright © 2016年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYLLaunchIntroductionDemo/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  LYLLaunchIntroductionDemo
+//
+//  Created by Lee on 16/4/7.
+//  Copyright © 2016年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
